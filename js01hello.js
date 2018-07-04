a316282fc60a224b0ea8d13975552a87772610e3
edu1004 <rf00000@naver.com>
2018-07-04 16:16:07 +0900

    second commit

---
+++ js01hello.js
@@ -0,0 +1,14 @@
+console.log("Hello Atom");
+
+class Test {
+  constructor() {
+
+  }
+  sum(){
+    return 100;
+  }
+}
+
+let t = new Test();
+let s = t.sum();
+console.log("s:",s);
---
+++ js02request.js
@@ -0,0 +1,9 @@
+console.log("Hello request");
+var request = require('request');
+//import request from 'request';
+request('http://yangssem.blog.me',
+        function (error, response, body) {
+  console.log('error:', error);
+  console.log('statusCode:', response && response.statusCode);
+  console.log('body:', body);
+});
